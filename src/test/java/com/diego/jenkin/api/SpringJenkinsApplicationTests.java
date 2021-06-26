c1ee3a881320f837fdd6ff411146a310a1ad73af
diego-virguez <915+diego-virguez@users.noreply.github.xpo.com>
2021-06-26 17:49:54 -0500

    falling again

--- src/test/java/com/diego/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/diego/jenkin/api/SpringJenkinsApplicationTests.java
@@ -11,7 +11,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 
-    public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
     @Test
     public void contextLoads() {
